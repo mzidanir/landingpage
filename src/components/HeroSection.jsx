263f4a60c99bcf7e35ce29d79bb1e617c54f13fd
mzidanir <repsone17@gmail.com>
2024-08-31 12:01:02 +0700

    feat: initial commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HeroSection() {
   return (
-    <section className="bg-cover bg-center h-screen text-white flex items-center justify-center" style={{ backgroundImage: "url('/yuda123.jpg')" }}>
+    <section className="bg-cover bg-center h-screen text-white flex items-center justify-center" style={{ backgroundImage: "url('yuda123.jpg')" }}>
       <div className="text-center">
         <h1 className="text-5xl font-bold">Masa Depan Teknologi Otomotif Bersama Tesla</h1>
         <p className="mt-4 text-xl">Inovasi yang Mengubah Dunia</p>
